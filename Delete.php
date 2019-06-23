907be3bfa72297f489b8598fb5c29a5b1397f446
kidproko2223 <44961169+kidproko2223@users.noreply.github.com>
2019-06-23 17:06:55 +0700

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<h1>DELETE DATA FROM DATABASE</h1>
+<h1>INSERT DATA TO DATABASE</h1>
 
 <?php
-echo "Delete database!";
+echo "Insert database!";
 ?>
 <ul>
     <form name="DeleteData" action="Delete.php" method="POST" >
@@ -19,46 +19,7 @@ if (empty(getenv("DATABASE_URL"))){
      
    $db = parse_url(getenv("DATABASE_URL"));
    $pdo = new PDO("pgsql:" . sprintf(
-        <!DOCTYPE html>
-<html>
-<body>
-
-<h1>DELETE DATA FROM DATABASE</h1>
-
-<?php
-echo "Delete database!";
-?>
-<ul>
-    <form name="DeleteData" action="DeleteData.php" method="POST" >
-<li>toyID:</li><li><input type="text" name="toyid" /></li>
-<li><button type="submit" value="Submit">Delete</button></li>
-<?php
-if (empty(getenv("DATABASE_URL"))){
-    echo '<p>The DB does not exist</p>';
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
-}  else {
-     
-   $db = parse_url(getenv("DATABASE_URL"));
-   $pdo = new PDO("pgsql:" . sprintf(
         "host=ec2-50-19-114-27.compute-1.amazonaws.com;port=5432;user=rkrhyqmezoltlx;password=67b7ce04b44c5507b8cf05e3be1a2b14ea968e575e90d3241569ea30d830e44d;dbname=d2b8o7aiucjc2b",
-        $db["host"],
-        $db["port"],
-        $db["user"],
-        $db["pass"],
-        ltrim($db["path"], "/")
-   ));
-}  
-$sql = "DELETE FROM toystore WHERE toyid = '$_POST[toyid]'";
-$stmt = $pdo->prepare($sql);
-if(is_null ($_POST[toyid])== FALSE)  {    
-    if($stmt->execute() == TRUE){
-        echo "Record updated successfully.";
-    } else {
-        echo "Error updating record. ";
-    }}
-?>
-</body>
-</html>
         $db["host"],
         $db["port"],
         $db["user"],
