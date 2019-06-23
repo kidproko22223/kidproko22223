493081475919832a2474c81db1a0e2e50747fa9b
kidproko2223 <44961169+kidproko2223@users.noreply.github.com>
2019-06-23 16:59:24 +0700

    Update ConnecttoDB.php

--- ConnecttoDB.php
+++ ConnecttoDB.php
@@ -1,32 +1,21 @@
 <!DOCTYPE html>
 <html>
-    <head>
-<title>Insert data to PostgreSQL with php - creating a simple web application</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<style>
-li {
-list-style: none;
-}
-</style>
-</head>
 <body>
-<h1>INSERT DATA TO DATABASE</h1>
-<h2>Enter data into ToyStore table</h2>
-<ul>
-    <form name="InsertData" action="Insert.php" method="POST" >
-<li>toyid:</li><li><input type="text" name="toyid" /></li>
-<li>toyname:</li><li><input type="text" name="toyname" /></li>
 
-<li><input type="submit" /></li>
-</form>
-</ul>
+<h1>DATABASE CONNECTION</h1>
+
+<?php
+ini_set('display_errors', 1);
+echo "Hello, this is ATN toy shop";
+?>
 
 <?php
 if (empty(getenv("DATABASE_URL"))){
     echo '<p>The DB does not exist</p>';
     $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
 }  else {
-     
+     echo '<p>The DB exists</p>';
+     echo getenv("dbname");
    $db = parse_url(getenv("DATABASE_URL"));
    $pdo = new PDO("pgsql:" . sprintf(
         "host=ec2-50-19-114-27.compute-1.amazonaws.com;port=5432;user=rkrhyqmezoltlx;password=67b7ce04b44c5507b8cf05e3be1a2b14ea968e575e90d3241569ea30d830e44d;dbname=d2b8o7aiucjc2b",
@@ -37,24 +26,43 @@ if (empty(getenv("DATABASE_URL"))){
         ltrim($db["path"], "/")
    ));
 }  
-if($pdo === false){
-     echo "ERROR: Could not connect Database";
-}
-$sql = "INSERT INTO store(toyid, toyname)"
-        . " VALUES('$_POST[toyid]','$_POST[toyname]')";
+$sql = "SELECT * FROM toystore";
 $stmt = $pdo->prepare($sql);
-//$stmt->execute();
- if (is_null($_POST[toyid])) {
-   echo "ToyId must be not null";
- }
- else
- {
-    if($stmt->execute() == TRUE){
-        echo "Record inserted successfully.";
-    } else {
-        echo "Error inserting record: ";
-    }
- }
+//Thi?t l?p ki?u d? li?u tr? v?
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$resultSet = $stmt->fetchAll();
+echo '<p>Students information:</p>';
 ?>
+<div id="container">
+<table class="table table-bordered table-condensed">
+    <thead>
+      <tr>
+        <th>Toy ID</th>
+        <th>Toy Name</th>
+
+
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      // t?o vòng l?p 
+         //while($r = mysql_fetch_array($result)){
+             foreach ($resultSet as $row) {
+      ?>
+   
+      <tr>
+        <td scope="row"><?php echo $row['toyid'] ?></td>
+        <td><?php echo $row['toyname'] ?></td>
+
+        
+      </tr>
+     
+      <?php
+        }
+      ?>
+    </tbody>
+  </table>
+</div>
 </body>
 </html>
